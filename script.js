e05666cab10a87455fad5c8eaf6113c520b931da
SlipperyySlope <mlpfursuit@gmail.com>
2024-09-09 06:01:23 -0700

    Initial setup

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('card1').addEventListener('click', function() {
+    this.classList.toggle('is-flipped');
+});
+
+document.getElementById('card2').addEventListener('click', function() {
+    this.classList.toggle('is-flipped');
+});
+
+document.getElementById('card3').addEventListener('click', function() {
+    this.classList.toggle('is-flipped');
+});
+
+document.getElementById('card4').addEventListener('click', function() {
+    this.classList.toggle('is-flipped');
+});
